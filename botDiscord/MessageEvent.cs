8e9de105057fd06c5bb61454bc06c4505736bdbd
jetonpeche <nicolas.np63@gmail.com>
2026-02-05 22:20:46 +0100

    bot discord => lecture des messages (DEBUT)

---
+++ botDiscord/MessageEvent.cs
@@ -0,0 +1,16 @@
+﻿using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+using NetCord.Rest;
+
+namespace botDiscord;
+
+public class MessageEvent(HttpClient httpClient): IMessageCreateGatewayHandler
+{
+     public async ValueTask HandleAsync(Message message)
+     {
+          Console.WriteLine($"Message de {message.Author?.Username}");
+          var b = httpClient.BaseAddress;
+
+          await message.AddReactionAsync(new ReactionEmojiProperties("✅"));
+     }
+}
---
+++ botDiscord/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDiscordGateway()
+     .AddGatewayHandlers(typeof(Program).Assembly)
+     .AddHttpClient("", x =>
+     {
+          x.BaseAddress = new Uri("http://localhost:5256/api");
+     });
+
+var host = builder.Build();
+
+await host.RunAsync();
