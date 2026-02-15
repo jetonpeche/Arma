83eb94eb6e0a7a783d28dfc8ee9ab48d4df59d80
jetonpeche <nicolas.np63@gmail.com>
2026-02-15 14:45:04 +0100

    Create VaisseauPosseder.cs

---
+++ back/back/Models/VaisseauPosseder.cs
@@ -0,0 +1,14 @@
+﻿using LiteDB;
+
+namespace back.Models;
+
+public sealed class VaisseauPosseder
+{
+     [BsonId]
+     public int Id { get; set; }
+
+     [BsonRef]
+     public Vaisseau Vaisseau { get; set; } = null!;
+
+     public string? Information { get; set; }
+}
