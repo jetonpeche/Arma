4fb3c209ed4261bcb518f2f25b865d2544ae9922
jetonpeche <nicolas.np63@gmail.com>
2026-07-05 22:13:59 +0200

    fix acces pae flotte + msg travaux

--- front/src/app/guards/connecter-guard.ts
+++ front/src/app/guards/connecter-guard.ts
@@ -12,7 +12,8 @@ export const connecterGuard: CanActivateFn = (route, state) =>
         state.url == EUrl.Boutique || state.url == EUrl.Personnage || 
         state.url == EUrl.Specialite || state.url == EUrl.Medaille || 
         state.url == EUrl.HistoriqueCampagne || state.url == EUrl.Grade ||
-        state.url == EUrl.PlaneteOrigine || state.url == "/cimetiere"
+        state.url == EUrl.PlaneteOrigine || state.url == "/cimetiere" || 
+        state.url == "/flotte"
     )
         return true;
 
