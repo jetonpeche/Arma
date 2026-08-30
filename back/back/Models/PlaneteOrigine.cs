5ce1fde3ebd189bdfce9f7ad073f8193027b0655
jetonpeche <nicolas.np63@gmail.com>
2026-08-30 19:16:08 +0200

    Update PlaneteOrigine.cs

--- back/back/Models/PlaneteOrigine.cs
+++ back/back/Models/PlaneteOrigine.cs
@@ -10,11 +10,26 @@ public sealed class PlaneteOrigine
      [BsonRef]
      public Systeme Systeme { get; set; } = null!;
 
-    public string Nom { get; set; } = null!;
+     public string Nom { get; set; } = null!;
      public string? Description { get; set; }
-    public string? NomFichier { get; set; }
-    public int Statut { get; set; }
-    public bool EstPlaneteOrigine { get; set; }
+     public string? NomFichier { get; set; }
+
+     /// <summary>
+     /// Statut de l'astre (Vitrifié, en guerre...)
+     /// </summary>
+     public int Statut { get; set; }
+
+     /// <summary>
+     /// A qui appartient l'astre (UNSC, covenante...)
+     /// </summary>
+     public int Appartenance { get; set; }
+
+     /// <summary>
+     /// Type d'astre
+     /// Lune, planete, asteroide, etoile
+     /// </summary>
+     public int Type { get; set; }
+     public bool EstPlaneteOrigine { get; set; }
 
     public decimal PositionX { get; set; }
     public decimal PositionY { get; set; }
